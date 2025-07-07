587a751f026eb501947cc1d174359f6a3bc27a2a
Imran Ahmed <imran.softwindtech@gmail.com>
2025-07-07 16:20:16 +0600

    header and footer design added

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+       maxWidth: {
+        '1350': '1350px', 
+        '1300': '1300px', 
+      },
+    },
+   
+  },
+  plugins: [],
+}
